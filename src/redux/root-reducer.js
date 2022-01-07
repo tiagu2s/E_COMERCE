5ac3d9759af30ffd784960abd4576f5532ef2ee4
Tiago Lopes <36971635+tiagu2s@users.noreply.github.com>
2022-01-07 11:32:33 +0000

    Adding redux persist tlib to cart, since we wanna keep the state of cart on local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,21 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+//TELLS TO USE LOCAL STORAGE
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key:'root',
+  storage,
+  // reduce to persist
+  whitelist:['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-});
+})
+
+
+export default persistReducer(persistConfig, rootReducer)
